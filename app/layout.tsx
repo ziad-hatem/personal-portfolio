6298a6a9604cbffd401c709d93bd482be953bba8
Ziad Hatem <109518062+ziad-hatem@users.noreply.github.com>
2024-08-26 20:23:49 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
-      <meta name="google-site-verification" content="XN7w7kIuu1tT4G5lbFaqfuB59094mdgGm9uHleu4v84" />
+      <meta name="google-site-verification" content="8vvXACQeiioH9qtrZe1FfNcOuL_8DAwTn01qM97vtHs" />
       </head>
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
